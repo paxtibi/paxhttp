529ad5e24fd347ba0b7a70a8badfefc82dbf6e0f
PaxTibi <marcoroin@gmail.com>
2019-11-19 13:31:00 +0100

    Simplest demo Server

---
+++ demos/pck/stoppableserver.pas
@@ -0,0 +1,60 @@
+program stoppableserver;
+{$D+}
+{$mode objfpc}{$H+}
+
+uses {$IFDEF UNIX} {$IFDEF UseCThreads}
+  cthreads, {$ENDIF} {$ENDIF}
+  Classes,
+  SysUtils,
+  CustApp,
+  paxhttp.Messages,
+  custhttpapp,
+  HTTPDefs,
+  paxhttp.server;
+
+{******************************************************************************}
+type
+  { TStoppableServer }
+  {****************************************************************************}
+  TStoppableServer = class(TCustomSlimHttpApplication)
+  protected
+    procedure DoServerStop(aReq: TRequest; aResp: TResponse; args: TStrings);
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+  end;
+  {****************************************************************************}
+
+var
+  Application: TStoppableServer;
+
+  {****************************************************************************}
+  procedure TStoppableServer.DoServerStop(aReq: TRequest; aResp: TResponse; args: TStrings);
+  begin
+    aResp.Content := 'Bye!';
+    Application.Terminate;
+  end;
+
+  {****************************************************************************}
+  constructor TStoppableServer.Create(AOwner: TComponent);
+  begin
+    inherited Create(AOwner);
+    AddRoute('GET', '/server-stop', @DoServerStop);
+  end;
+
+  {****************************************************************************}
+  destructor TStoppableServer.Destroy;
+  begin
+    inherited Destroy;
+  end;
+
+  {****************************************************************************}
+begin
+  Application      := TStoppableServer.Create(nil);
+  Application.Title := 'Stoppable Server';
+  Application.Port := 3000;
+  Application.Run;
+  Application.Free;
+end.
+{******************************************************************************}
+
